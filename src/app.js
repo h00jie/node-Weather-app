a658eddd8e887d0b7f3decdcf05b809de89bf64c
Panagiotis Chatzikonstantinou <panos.hd@gmail.com>
2020-06-23 01:05:51 +0300

    Setup up for heroku

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const { env } = require('process')
 
 const app = express()
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT
 
 // Define paths for Express config
 const publicDirPath = path.join(__dirname, '../public')
